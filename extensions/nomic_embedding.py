053a9d7109401aceddff825c0be22dc997059c76
Amila Rathnayake <amila.rathnayake@gm.com>
2025-03-14 15:39:40 -0400

    Added chroma vector store

--- extensions/nomic_embedding.py
+++ extensions/nomic_embedding.py
@@ -1,22 +1,21 @@
-from nomic import embed
 import numpy as np
+import asyncio
+from nomic import embed
 from llama_index.core.base.embeddings.base import BaseEmbedding, Embedding
 
 # nk-ALQs3L7funfp5oHby8uhuocAlmLHkezUteXLg7eBJ8A
 class NomicEmbedding(BaseEmbedding):
     def _get_query_embedding(self, query: str) -> Embedding:
-        print("_get_query_embedding -------- ")
         output= embed.text(
                             texts=[query],
                             model='nomic-embed-text-v1.5',
                             task_type='search_query',
-                            inference_mode='local'
+                            inference_mode='local',
                           )
         
-        return np.array(output["embeddings"][0])
+        return output["embeddings"][0]
 
     def _get_text_embedding(self, text: str) -> Embedding:
-        print("_get_text_embedding -------- ")
         output= embed.text(
                             texts=[text],
                             model='nomic-embed-text-v1.5',
@@ -24,7 +23,8 @@ class NomicEmbedding(BaseEmbedding):
                             inference_mode='local'
                           )
          
-        return np.array(output["embeddings"][0])
+        return output["embeddings"][0]
     
     async def _aget_query_embedding(self, query: str) -> Embedding:
-        pass
+        result = await asyncio.to_thread(self._get_query_embedding, query)
+        return result
--- main.py
+++ main.py
@@ -10,48 +10,70 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.llms.mock import MockLLM
 from llama_index.llms.ollama import Ollama
 
+from llama_index.core import VectorStoreIndex
+from llama_index.core import StorageContext
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
 from huggingface_hub import login
 
-# nk-ALQs3L7funfp5oHby8uhuocAlmLHkezUteXLg7eBJ8A
 print("RAG starting")
 
-# Load custom embedding 
+# Load custom embedding. Custom NomicEmbedding because it light weight.
 embed_model = NomicEmbedding();
-#embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5", local_files_only=True)
 print("Loaded custom embedding model")
 
-documents = SimpleDirectoryReader("./docs").load_data()
-for doc in documents:
-    print(doc)
+# Initialize the Chroma client
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
 
-splitter = SentenceSplitter(chunk_size=200)  # Experiment with chunk size
-nodes = splitter.get_nodes_from_documents(documents)
-print("Number of nodes {}".format(len(nodes)))
+collection_name = "random_facts_collection"
+collections = chroma_client.list_collections()
+collection_names = [col.name for col in collections]
 
-index = VectorStoreIndex(nodes=nodes,
-                        embed_model=embed_model)
+index = None
+if  not collection_name in collection_names:
+    print("Collection {} was not found. Creating ... ".format(collection_name))
+
+    documents = SimpleDirectoryReader("./docs").load_data()
+    
+    # Choose better chunk size for breaking the text. 
+    # If too big context text match size will be too big.
+    splitter = SentenceSplitter(chunk_size=200) 
+    nodes = splitter.get_nodes_from_documents(documents)
+
+    # Create collection
+    chroma_collection = chroma_client.create_collection(collection_name)
+
+    # Wrap Chroma in a LlamaIndex-compatible vector store
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex(nodes=nodes,
+                            embed_model=embed_model,
+                            storage_context=storage_context)
+else:
+    chroma_collection = chroma_client.get_collection(collection_name)
+
+    print("Collection {} is already in the data base".format(collection_name))
+
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    index = VectorStoreIndex.from_vector_store(vector_store=vector_store,
+                                               embed_model=embed_model);
 
 # Create the llm instance
+#mock_llm_model=MockLLM()
 llm_model=Ollama(model="llama3.2", request_timeout=360.0)
-mock_llm_model=MockLLM()
 print("Loaded LLM model")
 
-query_engine = index.as_query_engine(similarity_top_k=1, llm=mock_llm_model)
+query_engine = index.as_query_engine(similarity_top_k=1, llm=llm_model)
 
 async def search_documents(query: str) -> str:
     print("Searching document index for query {}".format(query))
     response = await query_engine.aquery(query)
     return str(response)
 
-agent = AgentWorkflow.from_tools_or_functions(
-    [search_documents],
-    llm=llm_model,
-    system_prompt="""You are a helpful assistant that can route questions to search_documents and get the answer""",
-)
-
 async def main():
     # Run the agent
-    response = await search_documents("Amila?")
+    response = await search_documents("Where done Amila lives?")
     print(str(response))
 
 # Run the agent
